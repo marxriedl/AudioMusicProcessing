622b33a865ab8a1802c26c1ac04f3c83b6ee57c4
marxriedl <marxriedl@gmail.com>
2013-04-29 16:18:01 +0200

    Added EvalData

---
+++ at.cp.jku.teaching.amprocessing/src/at/cp/jku/teaching/amprocessing/project/HFCOnsetProcessor.java
@@ -0,0 +1,14 @@
+package at.cp.jku.teaching.amprocessing.project;
+
+import at.cp.jku.teaching.amprocessing.Processor;
+
+public class HFCOnsetProcessor extends Processor {
+
+	public HFCOnsetProcessor(String filename) {
+		super(filename);
+	}
+
+	@Override 
+	public void analyze() {
+	}
+}
